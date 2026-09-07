122d36a84feb284b7e5cc82d3b5253a428e089bb
ahmed <ahmedmohamed3dss@gmail.com>
2026-09-07 20:13:46 +0300

    Add project files.

---
+++ C sharp 2# assignment/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace C_sharp_2__assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
