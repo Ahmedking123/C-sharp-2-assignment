5807406d119c258ea9842af8823df89193e1a97b
ahmed <ahmedmohamed3dss@gmail.com>
2026-09-07 20:30:21 +0300

    First question

--- C sharp 2# assignment/Program.cs
+++ C sharp 2# assignment/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace C_sharp_2__assignment
+﻿using System.Reflection;
+
+namespace C_sharp_2__assignment
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region First question
+
+            string title = "clean code";
+            string upperTitle = title.ToUpper();
+            Console.WriteLine(title);
+            Console.WriteLine(upperTitle);
+
+            #endregion
         }
     }
 }
